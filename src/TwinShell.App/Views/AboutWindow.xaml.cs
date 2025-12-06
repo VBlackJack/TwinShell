fa4b28219574ee6edb40c25bac4ffb60334d270e
VBlackJack <veryblackjack@proton.me>
2025-12-06 09:38:58 +0100

    fix: Dynamic version in About window from assembly

--- src/TwinShell.App/Views/AboutWindow.xaml.cs
+++ src/TwinShell.App/Views/AboutWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Windows;
 
 namespace TwinShell.App.Views;
@@ -10,6 +11,10 @@ public partial class AboutWindow : Window
     public AboutWindow()
     {
         InitializeComponent();
+
+        // Get version from assembly
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+        VersionText.Text = $"Version {version?.Major}.{version?.Minor}.{version?.Build}";
     }
 
     private void CloseButton_Click(object sender, RoutedEventArgs e)
